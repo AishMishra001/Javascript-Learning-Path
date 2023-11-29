9f93b428adbad405f7b08efab4496ce5c7a682ee
Aish Mishra <aishlunatic001@gmail.com>
2023-11-29 21:52:49 +0530

    ADVANCE FUNCTIONS 1 ENTRY

---
+++ Scripts/12_Todo_List.js
@@ -0,0 +1,55 @@
+let Todolist=[
+  {
+    name:'Watch youtube',
+    dueDate:'2022-12-22'
+},
+{
+  name:'clean dishes',
+  dueDate:'2022-12-22'
+}];
+renderTodolist();
+function renderTodolist(){
+
+  let todolistHtml='';
+  Todolist.forEach(function(todoObject,index){
+   
+    const{name,dueDate} = todoObject ;
+
+    const html =
+    `<div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+    Todolist.splice(${index},1);
+    renderTodolist();
+    " class="delete-button">Delete</button>
+    `;
+    todolistHtml = todolistHtml + html ; 
+
+
+  })
+
+
+  document.querySelector('.js-todo-show').innerHTML = todolistHtml; 
+  
+}
+
+
+
+
+function Todoadd(){
+  const nameInputElement = document.querySelector('.js-name-button');
+  const name = nameInputElement.value ; 
+
+  const dateInputElement = document.querySelector('.js-due-date');
+  const dueDate = dateInputElement.value ; 
+
+  Todolist.push({
+    // name:name , If the property and the variable name is same we can use a shortcut by using just : name 
+    name ,   // shorthand property 
+    // dueDate : dueDate 
+    dueDate,   // shorthand property 
+  });
+  
+  nameInputElement.value ='';
+  renderTodolist();
+}
--- Scripts/12_rock-paper-scissors.js
+++ Scripts/12_rock-paper-scissors.js
@@ -14,6 +14,24 @@ updateScoreElement();
  }
  */
 // !score is same as score === null 
+let isAutoPlaying = false;
+let intervalId ; 
+function autoPlay(){
+  
+  if(!isAutoPlaying){
+   intervalId =  setInterval(function(){
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    },1000);
+    isAutoPlaying = true ; 
+  }
+  else{
+     clearInterval(intervalId);
+     isAutoPlaying = false ; 
+  }
+
+  
+}
 
 
 function playGame(playerMove) {
